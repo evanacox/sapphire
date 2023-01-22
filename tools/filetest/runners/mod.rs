e2b8bc0d9e36a14c8355ff7565749fd0dfdad4a3
Evan Cox <evanacox00@gmail.com>
2023-01-21 22:41:19 -0500

    [filetest] revamp filetest, enable much more complex tests

---
+++ tools/filetest/runners/mod.rs
@@ -0,0 +1,13 @@
+//======---------------------------------------------------------------======//
+//                                                                           //
+// Copyright 2022 Evan Cox <evanacox00@gmail.com>. All rights reserved.      //
+//                                                                           //
+// Use of this source code is governed by a BSD-style license that can be    //
+// found in the LICENSE.txt file at the root of this project, or at the      //
+// following link: https://opensource.org/licenses/BSD-3-Clause              //
+//                                                                           //
+//======---------------------------------------------------------------======//
+
+mod parse;
+
+pub use parse::*;
