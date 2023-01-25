a7bc3e7f62adeb62ea68b223085036b0788b21e5
Evan Cox <evanacox00@gmail.com>
2023-01-25 10:10:31 -0500

    [copyright] update copyright years

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,11 @@
 //======---------------------------------------------------------------======//
-// Copyright (c) 2022 Evan Cox <evanacox00@gmail.com>.                       //
 //                                                                           //
-// Licensed under the Apache License, Version 2.0 (the "License");           //
-// you may not use this file except in compliance with the License.          //
-// You may obtain a copy of the License at                                   //
+// Copyright 2022-2023 Evan Cox <evanacox00@gmail.com>. All rights reserved. //
 //                                                                           //
-//     http://www.apache.org/licenses/LICENSE-2.0                            //
+// Use of this source code is governed by a BSD-style license that can be    //
+// found in the LICENSE.txt file at the root of this project, or at the      //
+// following link: https://opensource.org/licenses/BSD-3-Clause              //
 //                                                                           //
-// Unless required by applicable law or agreed to in writing, software       //
-// distributed under the License is distributed on an "AS IS" BASIS,         //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
-// See the License for the specific language governing permissions and       //
-// limitations under the License.                                            //
 //======---------------------------------------------------------------======//
 
 #![allow(dead_code)]
